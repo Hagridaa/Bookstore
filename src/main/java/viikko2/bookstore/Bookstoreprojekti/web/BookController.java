32604bc1878d567b8ad423f621b0a7741bb1a502
hagridaa <annekristiinarasanen@gmail.com>
2019-09-15 14:53:49 +0300

    edit functionality

--- src/main/java/viikko2/bookstore/Bookstoreprojekti/web/BookController.java
+++ src/main/java/viikko2/bookstore/Bookstoreprojekti/web/BookController.java
@@ -2,8 +2,10 @@ package viikko2.bookstore.Bookstoreprojekti.web;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -61,9 +63,34 @@ public class BookController {
 				return "booklist"; // DispatherServlet saa tämän template-nimen ja kutsuu seuraavaksi booklist.html-templatea,
 									// joka prosessoidaan palvelimella
 }
+		//kirjan poisto
 		@RequestMapping(value = "delete/{id}", method = RequestMethod.GET)
-		public String deleteBook(@PathVariable("id") Long BookId, Model model) {
-			bookRepository.deleteById(BookId);
+		public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+			bookRepository.deleteById(bookId);
 			return "redirect:../booklist";
 		}
+		
+		// now you will add current book object to model. You also have to send current
+		//book id from the list page to controller (like you did in delete link)
+		
+		// /edit
+		//haetaan annettu id kuten delete toiminnossa getillä
+		@RequestMapping(value = "edit/{id}", method = RequestMethod.GET)
+		public String editBook(@PathVariable("id") Long bookId, Model model) {
+			//etsitään id:n avulla muokattavan kirjan tiedot
+			//Optional vaihtoehdolla voi tehdä niin, että jos tietoa ei löydykkään niin sovellus ei kaadu
+			Optional<Book> foundedBook = bookRepository.findById(bookId);
+			model.addAttribute("foundedbook", foundedBook); //// välitetään lista templatelle model-olion avulla
+			return "edit";
+		}
+		//Päivitys, haetaan kannasta tiedot ja takaisin update komennolla ei inserttinä ette itee uutta
+		//Lomakkeen tietojen vastaanotto ja tallennus kantaan
+		@RequestMapping(value = "/editbook",method = RequestMethod.POST)
+		public String update(Book book) {
+			if
+			(bookRepository.existsById(book.getId())) {
+					bookRepository.save(book);
+			}
+						return "redirect:../booklist";
+}
 }
